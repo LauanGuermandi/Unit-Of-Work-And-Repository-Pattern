28742bdce94d9a255a612a6745ca54f1edd72974
Lauan Borges Guermandi <lbguermandi@paschoalotto.com.br>
2021-10-17 03:16:17 -0300

    feat: add context and migrations

---
+++ src/PocketBook.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PocketBook.Domain;
+
+namespace PocketBook.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
